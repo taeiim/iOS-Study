f7f991bcb001bad1f4e20260e29d40f3b42e65ae
taeiim <taeiim.dev@gmail.com>
2020-06-26 21:27:15 +0900

    [TableViewPracticeAlone] Create Project

---
+++ TableViewPracticeAlone/TableViewPracticeAlone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPracticeAlone
+//
+//  Created by 박태임 on 2020/06/26.
+//  Copyright © 2020 taeiim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
